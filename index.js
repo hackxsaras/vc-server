ed783d01e9e7de21c51f139e9b3b59bf6fb75cf4
unknown <hackxsaras@gmail.com>
2023-09-13 12:16:16 +0530

    config port

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const { PeerServer } = require("peer");
-
-const peerServer = PeerServer({ port: 9000, path: "/myapp" });
+let port = 9000;
+if(process.env && process.env.PORT) port = process.env.PORT;
+const peerServer = PeerServer({ port, path: "/myapp" });
